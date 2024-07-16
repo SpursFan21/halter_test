adfe69ad0e693049dd0dc37640335bebb8aa88e2
Duncan Turner <duncanfrederic@gmail.com>
2024-07-16 18:03:38 +1200

    main and writer refractored - to match format used by producer and analyser

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
-// main.go
-
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/SpursFan21/halter_test/analyser"
 	"github.com/SpursFan21/halter_test/producer"
 	"github.com/SpursFan21/halter_test/writer"
-	"github.com/jmoiron/sqlx"
 )
 
 func main() {
 	args := os.Args[1:]
-	if len(args) < 1 {
-		println("Usage: go run main.go [producer|analyser|writer]")
-		os.Exit(1)
-	}
-
 	app := args[0]
 
 	switch app {
@@ -29,47 +18,7 @@ func main() {
 	case "analyser":
 		analyser.Analyser()
 	case "writer":
-		conn, ch, msgs, err := writer.ConnectRabbitMQ()
-		if err != nil {
-			log.Fatalf("Error connecting to RabbitMQ: %v", err)
-		}
-		defer conn.Close()
-		defer ch.Close()
-
-		// Get PostgreSQL connection details from environment variables
-		dbHost := os.Getenv("DATABASE_HOST")
-		dbPort := os.Getenv("DATABASE_PORT")
-		dbUser := os.Getenv("DATABASE_USER")
-		dbPassword := os.Getenv("DATABASE_PASSWORD")
-		dbName := os.Getenv("DATABASE_DBNAME")
-
-		// Connect to PostgreSQL
-		db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			dbHost, dbPort, dbUser, dbPassword, dbName))
-		if err != nil {
-			log.Fatalf("Error connecting to PostgreSQL: %v", err)
-		}
-		defer db.Close()
-
-		// Consume messages from RabbitMQ
-		for msg := range msgs {
-			var message writer.Message
-			err := json.Unmarshal(msg.Body, &message)
-			if err != nil {
-				log.Printf("Failed to decode message: %v", err)
-				continue
-			}
-
-			// Handle the message: update or insert into the database
-			err = writer.UpdateDatabase(db, message)
-			if err != nil {
-				log.Printf("Failed to update database: %v", err)
-				continue
-			}
-
-			log.Printf("Updated database with message: %+v", message)
-		}
-
+		writer.Writer()
 	default:
 		println("Invalid application name. Use 'producer', 'analyser', or 'writer'.")
 		os.Exit(1)
--- writer/writer.go
+++ writer/writer.go
@@ -3,12 +3,14 @@
 package writer
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +19,38 @@ type Message struct {
 	Timestamp    time.Time `json:"timestamp"`
 	Latitude     float64   `json:"latitude"`
 	Longitude    float64   `json:"longitude"`
+}
+
+func ConnectPostgres() (*sqlx.DB, error) {
+	maxAttempts := 5
+
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_DBNAME")
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var db *sqlx.DB
+	var err error
+
+	for i := 0; i < maxAttempts; i++ {
+		if i != 0 {
+			time.Sleep(5 * time.Second)
+		}
+		db, err = sqlx.Connect("postgres", psqlInfo)
+		if err != nil {
+			continue
+		}
+		err = db.Ping()
+		if err != nil {
+			continue
+		}
+		return db, nil
+	}
+
+	return nil, fmt.Errorf("failed to connect to PostgreSQL after %d attempts", maxAttempts)
 }
 
 func ConnectRabbitMQ() (*amqp.Connection, *amqp.Channel, <-chan amqp.Delivery, error) {
@@ -62,14 +96,12 @@ func UpdateDatabase(db *sqlx.DB, message Message) error {
 	var existingMessage Message
 	err := db.Get(&existingMessage, "SELECT * FROM locations WHERE serial_number=$1", message.SerialNumber)
 	if err != nil {
-		// Assuming error means no row found, insert the new message
 		_, err := db.Exec("INSERT INTO locations (serial_number, timestamp, latitude, longitude) VALUES ($1, $2, $3, $4)",
 			message.SerialNumber, message.Timestamp, message.Latitude, message.Longitude)
 		if err != nil {
 			return fmt.Errorf("failed to insert message into database: %w", err)
 		}
 	} else {
-		// Update existing message if timestamp is newer
 		if message.Timestamp.After(existingMessage.Timestamp) {
 			_, err := db.Exec("UPDATE locations SET timestamp=$1, latitude=$2, longitude=$3 WHERE serial_number=$4",
 				message.Timestamp, message.Latitude, message.Longitude, message.SerialNumber)
@@ -80,4 +112,36 @@ func UpdateDatabase(db *sqlx.DB, message Message) error {
 	}
 
 	return nil
+}
+
+func Writer() {
+	conn, ch, msgs, err := ConnectRabbitMQ()
+	if err != nil {
+		log.Fatalf("Error connecting to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	defer ch.Close()
+
+	db, err := ConnectPostgres()
+	if err != nil {
+		log.Fatalf("Error connecting to PostgreSQL: %v", err)
+	}
+	defer db.Close()
+
+	for msg := range msgs {
+		var message Message
+		err := json.Unmarshal(msg.Body, &message)
+		if err != nil {
+			log.Printf("Failed to decode message: %v", err)
+			continue
+		}
+
+		err = UpdateDatabase(db, message)
+		if err != nil {
+			log.Printf("Failed to update database: %v", err)
+			continue
+		}
+
+		log.Printf("Updated database with message: %+v", message)
+	}
 }
